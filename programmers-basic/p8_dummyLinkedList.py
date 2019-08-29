2750ebe52ec4071234f419ce61a32ab56cb03ff0
wldnjszz1 <wldnjszz1@naver.com>
2019-08-29 12:27:33 +0900

    더미연결리스트

---
+++ programmers-basic/p8_dummyLinkedList.py
@@ -0,0 +1,74 @@
+class Node:
+
+    def __init__(self, item):
+        self.data = item
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.nodeCount = 0
+        self.head = Node(None)
+        self.tail = None
+        self.head.next = self.tail
+
+    def traverse(self):
+        result = []
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+            result.append(curr.data)
+        return result
+
+    def getAt(self, pos):
+        if pos < 0 or pos > self.nodeCount:
+            return None
+
+        i = 0
+        curr = self.head
+        while i < pos:
+            curr = curr.next
+            i += 1
+
+        return curr
+
+    def insertAfter(self, prev, newNode):
+        newNode.next = prev.next
+        if prev.next is None:
+            self.tail = newNode
+        prev.next = newNode
+        self.nodeCount += 1
+        return True
+
+    def insertAt(self, pos, newNode):
+        if pos < 1 or pos > self.nodeCount + 1:
+            return False
+
+        if pos != 1 and pos == self.nodeCount + 1:
+            prev = self.tail
+        else:
+            prev = self.getAt(pos - 1)
+        return self.insertAfter(prev, newNode)
+
+    def popAfter(self, prev):
+        if prev.next is None:
+            return None
+        if prev.next.next is None:
+            delete = prev.next
+            prev.next = None
+            self.tail = prev
+        else:
+            delete = prev.next
+            prev.next = prev.next.next
+        self.nodeCount -= 1
+        return delete.data
+
+    def popAt(self, pos):
+        if pos < 1 or pos > self.nodeCount:
+            raise IndexError
+        return self.popAfter(self.getAt(pos - 1))
+
+
+def solution(x):
+    return 0
