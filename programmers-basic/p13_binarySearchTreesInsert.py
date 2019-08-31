2462057853fc2aa79bc75aa394d5add1d5be2374
wldnjszz1 <wldnjszz1@naver.com>
2019-08-31 22:36:47 +0900

    이진탐색트리 삽입

---
+++ programmers-basic/p13_binarySearchTreesInsert.py
@@ -0,0 +1,68 @@
+class Node:
+
+    def __init__(self, key, data):
+        self.key = key
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def insert(self, key, data):
+        if key < self.key:
+            if self.left:
+                self.left.insert(key, data)
+            else:
+                self.left = Node(key, data)
+
+        elif key > self.key:
+            if self.right:
+                self.right.insert(key, data)
+            else:
+                self.right = Node(key, data)
+
+        else:
+            raise KeyError('잘못된 값 입력')
+
+    def inorder(self):
+        traversal = []
+        if self.left:
+            traversal += self.left.inorder()
+        traversal.append(self)
+        if self.right:
+            traversal += self.right.inorder()
+        return traversal
+
+    def lookup(self, key, parent=None):
+        if key < self.key:
+            if self.left:
+                return self.left.lookup(key, self)
+            else:
+                return None, None
+        elif key > self.key:
+            if self.right:
+                return self.right.lookup(key, self)
+            else:
+                return None, None
+        else:
+            return self, parent
+
+
+class BinSearchTree:
+
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key, data):
+        if self.root:
+            self.root.insert(key, data)
+        else:
+            self.root = Node(key, data)
+
+    def inorder(self):
+        if self.root:
+            return self.root.inorder()
+        else:
+            return []
+
+
+def solution(x):
+    return 0
